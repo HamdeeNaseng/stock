10e21c3e5346d685f60a5104cd50bb5f53088a51
Hamdee Naseng <hamdinaseng@gmail.com>
2024-03-14 13:59:31 +0700

    Update itemService.java

--- src/main/java/com/jsp/stock/service/itemService.java
+++ src/main/java/com/jsp/stock/service/itemService.java
@@ -1,5 +1,14 @@
 package com.jsp.stock.service;
 
-public class ItemService {
+import org.springframework.stereotype.Service;
 
+import com.jsp.stock.repository.ItemRepository;
+@Service
+public class ItemService {
+    private final ItemRepository itemRepository;
+    public ItemService(ItemRepository itemRepository){
+        this.itemRepository = itemRepository;
+    }
+
+    
 }
